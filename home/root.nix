61fa875ab8dd46ee971a51705b520921e001bdd9
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-03 18:20:37 +0000

    added git info to root

--- home/root.nix
+++ home/root.nix
@@ -10,4 +10,10 @@
   ];
   home.stateVersion = config.nixVersion;
   home.sessionVariables.XDG_RUNTIME_DIR = "/run/user/$UID";
+
+  programs.git = {
+  	enable = true;
+  	userName = "Enrico Stemmer";
+	userEmail = "stemmer.enrico@gmail.com";
+  };
 }
