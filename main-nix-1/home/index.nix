cf0805a3bb6c1361dcd5321e1c18f69dc2f9705a
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-14 11:42:49 +0200

    added main-nix-2 secrets

--- main-nix-1/home/index.nix
+++ main-nix-1/home/index.nix
@@ -14,7 +14,7 @@ in
       openssh = {
         authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAA/Iusb9djUIvujvzUhkjW7cKysbuNwJPNd/zjmZc+t"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3IVwWPgIK0goGqR7IoN9OE/TuAKHrXLZPOkByWdOII"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDAz2IRRlU5CN8TRnHnHD98R5CWSGHQBg9hxqeYARdoK"
         ];
       };
       isSystemUser = true;
