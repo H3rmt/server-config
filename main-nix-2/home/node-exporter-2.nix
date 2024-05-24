5bd9d5b7532c49d9a87831d1084ae2c14a472363
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-24 15:30:55 +0200

    fixed node exporter

--- main-nix-2/home/node-exporter-2.nix
+++ main-nix-2/home/node-exporter-2.nix
@@ -26,7 +26,7 @@ in
             -u 0:0 \
             --restart unless-stopped \
             docker.io/prom/node-exporter:${NODE_EXPORTER_VERSION} \
-            --path.rootfs=/host --collector.netdev.device-include --collector.processes --collector.systemd.unit-include --collector.ethtool.device-include
+            --path.rootfs=/host --collector.netdev --collector.processes --collector.systemd --collector.ethtool
 
         ${exporter.run}
       '';
