d7e659bd5d33ce8d6beb1f1b48d1612a95b725f2
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-28 12:38:13 +0200

    some tests

--- shared/usr.nix
+++ shared/usr.nix
@@ -1,79 +1,84 @@
-{ pkgs, home, lib, ... }: {
-  xdg.configFile."micro/bindings.json" = {
-    text = builtins.toJSON {
-      "Ctrl-j" = "command-edit:jump ";
-      "Ctrl-l" = "command-edit:goto ";
+{ pkgs, home, lib, config, ... }: {
+  options = {
+    exported-services = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "Services to export to prometheus";
     };
   };
-
-  programs.micro = {
-    enable = true;
-    settings = {
-      clipboard = "terminal";
-      hltaberrors = true;
-      mkparents = true;
-      reload = "auto";
-      relativeruler = true;
-      savecursor = true;
-      saveundo = true;
-      scrollbar = true;
-      tabstospaces = true;
+  config = {
+    xdg.configFile."micro/bindings.json" = {
+      text = builtins.toJSON {
+        "Ctrl-j" = "command-edit:jump ";
+        "Ctrl-l" = "command-edit:goto ";
+      };
     };
-  };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    autosuggestion.enable = true;
-    autocd = true;
-    history = {
-      size = 10000;
-      share = false;
+    programs.micro = {
+      enable = true;
+      settings = {
+        clipboard = "terminal";
+        hltaberrors = true;
+        mkparents = true;
+        reload = "auto";
+        relativeruler = true;
+        savecursor = true;
+        saveundo = true;
+        scrollbar = true;
+        tabstospaces = true;
+      };
     };
-    initExtra = ''
-      export EDITOR=micro
 
-      bindkey "^[[1;5C" forward-word;
-      bindkey "^[[1;5D" backward-word;
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+      autocd = true;
+      history = {
+        size = 10000;
+        share = false;
+      };
+      initExtra = ''
+        export EDITOR=micro
+
+        bindkey "^[[1;5C" forward-word;
+        bindkey "^[[1;5D" backward-word;
 
 
-      autoload -U promptinit; promptinit
-      zstyle ':prompt:pure:host' color cyan
-      zstyle ':prompt:pure:user:root' color red
-      # force display of user@host
-      export PROMPT_PURE_SSH_CONNECTION="1"
-      prompt pure
+        autoload -U promptinit; promptinit
+        zstyle ':prompt:pure:host' color cyan
+        zstyle ':prompt:pure:user:root' color red
+        # force display of user@host
+        export PROMPT_PURE_SSH_CONNECTION="1"
+        prompt pure
 
-      eval "$(zoxide init zsh)";
-      
-      alias cd="z";
-      alias ls="eza -a";
-      alias ll="eza -lahg --icons --git";
-      alias grep="rg";
-    '';
-    plugins = [ ];
-  };
-
-  exported-services = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
-    description = "Services to export to prometheus";
-  };
-
-  systemd.user.services.exporter = {
-    enable = lib.length exported-services > 0;
-     Unit = {
-      Description = "Service for Systemd Exporter";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-    Service = {
-      ExecStart = ''
-        ${pkgs.prometheus-systemd-exporter}/bin/systemd_exporter \
-          --web.listen-address ${config.address.private.systemd-exporter."${name}"} --systemd.collector.user \
-          --systemd.collector.unit-include=${lib.concatStringsSep "|" exported-services}
+        eval "$(zoxide init zsh)";
+        
+        alias cd="z";
+        alias ls="eza -a";
+        alias ll="eza -lahg --icons --git";
+        alias grep="rg";
       '';
+      plugins = [ ];
+    };
+
+
+
+    systemd.user.services.exporter = {
+      enable = lib.length config.exported-services > 0;
+      Unit = {
+        Description = "Service for Systemd Exporter";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = ''
+          ${pkgs.prometheus-systemd-exporter}/bin/systemd_exporter \
+            --web.listen-address ${config.address.private.systemd-exporter."${config.home.name}"} --systemd.collector.user \
+            --systemd.collector.unit-include=${lib.concatStringsSep "|" config.exported-services}
+        '';
+      };
     };
   };
 }
