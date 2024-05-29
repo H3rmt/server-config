3aec2c534fc2e31e36db5528b4653ae12a6cd261
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-30 01:58:21 +0200

    fixed logs

--- main-nix-2/home/reverseproxy.nix
+++ main-nix-2/home/reverseproxy.nix
@@ -93,6 +93,7 @@ in
             -v ${config.data-prefix}/letsencrypt:/etc/letsencrypt:ro \
             -v ${config.data-prefix}/website:${WEBSITE_PATH}:ro \
             -v nginx-cache:/var/cache/nginx/:U \
+            -v logs:/var/log/nginx/:U \
             --restart on-failure:10 \
             docker.io/h3rmt/nginx-http3-br:${NGINX_VERSION}
         
@@ -166,7 +167,7 @@ in
         worker_rlimit_nofile 8192;
         
         error_log /var/log/nginx/error.log notice;
-        pid /var/run/nginx.pid;
+        pid /tmp/nginx.pid;
         
         events {
           worker_connections 2048;
@@ -418,7 +419,7 @@ in
         log_format main '$remote_addr - $remote_user [$time_local] "$request" '
                         '$status $body_bytes_sent "$http_referer" '
                         '"$http_user_agent" "$http_x_forwarded_for"';
-        access_log /var/log/nginx/access.log main;
+        access_log /var/log/nginx/access.log main; 
         sendfile   on;
         tcp_nopush on;
 
