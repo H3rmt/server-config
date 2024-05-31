bdd4c74372310373ab937bb2429d5846eb95b1ff
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-05-31 16:42:21 +0200

    fixing reverseproxy redirect

--- main-nix-2/home/reverseproxy.nix
+++ main-nix-2/home/reverseproxy.nix
@@ -400,7 +400,7 @@ in
         location @goauthentik_proxy_signin {
           internal;
           add_header Set-Cookie $auth_cookie;
-          return 302 /outpost.goauthentik.io/start?rd=$request_uri;
+          return 302 http://${config.sites.authentik}/outpost.goauthentik.io/start?rd=$request_uri;
         }
       '';
     };
