f4b8d9176c8e4c3a2b38f9923738eaf699e85057
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-06-09 17:52:07 +0200

    added snowlake user

---
+++ main-nix-1/home/snowflake.nix
@@ -0,0 +1,45 @@
+{ age, clib }: { lib, config, home, pkgs, inputs, ... }:
+let
+  SNOWFLAKE_VERSION = "v2.8.1";
+in
+{
+  imports = [
+    ../../shared/usr.nix
+  ];
+
+  home.file = clib.create-files config.home.homeDirectory {
+    "up.sh" = {
+      executable = true;
+      text = ''
+        podman pod create --name=${config.pod-name} --userns=keep-id \
+            -p ${config.exporter.port} \
+            --network pasta:-a,172.16.0.1
+
+        podman run --name=snowflake-proxy-1 -d --pod=${PODNAME} \
+            --restart on-failure:10 \
+            -u $UID:$GID \
+            docker.io/thetorproject/snowflake-proxy:${SNOWFLAKE_VERSION} \
+            -ephemeral-ports-range 20000:40000 -unsafe-logging -summary-interval 6h0m0s
+
+        podman run --name=snowflake-proxy-2 -d --pod=${PODNAME} \
+            --restart on-failure:10 \
+            -u $UID:$GID \
+            docker.io/thetorproject/snowflake-proxy:${SNOWFLAKE_VERSION} \
+            -ephemeral-ports-range 40000:60000 -unsafe-logging -summary-interval 6h0m0s
+
+        ${exporter.run}
+      '';
+    };
+
+    "down.sh" = {
+      executable = true;
+      text = ''
+        podman stop -t 10 snowflake-proxy-1
+        podman stop -t 10 snowflake-proxy-2
+        podman rm snowflake-proxy-1 snowflake-proxy-2
+        ${config.exporter.stop}
+        podman pod rm ${config.pod-name}
+      '';
+    };
+  };
+}
