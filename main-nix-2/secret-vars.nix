65d02a3685faaf9a16fe3ca53a5320d228f0fc77
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-06-18 12:10:24 +0200

    added wg private key

--- main-nix-2/secret-vars.nix
+++ main-nix-2/secret-vars.nix
@@ -8,6 +8,10 @@
       file = ./secrets/root_pass.age;
       owner = "root";
     };
+    wireguard_private = {
+      file = ./secrets/wireguard_private.age;
+      owner = "root";
+    };
     reverseproxy_hetzner_token = {
       file = ./secrets/reverseproxy/hetzner_token.age;
       owner = "reverseproxy";
