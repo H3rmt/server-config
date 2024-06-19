be8e8accc735df6d16f5d75f0cc9f4bca4bb3ff5
Enrico Stemmer <stemmer.enrico@gmail.com>
2024-06-19 22:31:41 +0200

    fixed secrets for raspi

--- raspi-1/secrets/secrets.nix
+++ raspi-1/secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKIpoY7xkKbUMJ1/Fg1jPu1jwQzfXgjvybcsXnbI0eM";
+  main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIChc0OADBHo5eqE4tcVHglCGzUvHSTZ6LeC0RcGQ9V6C";
   my = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAA/Iusb9djUIvujvzUhkjW7cKysbuNwJPNd/zjmZc+t";
 in
 {
