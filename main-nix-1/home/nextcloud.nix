1a9d06d3f8369126185da128b493ff74b2a04182
Enrico Stemmer <enrico@h3rmt.zip>
2024-08-12 00:40:58 +0200

    fix fmt

--- main-nix-1/home/nextcloud.nix
+++ main-nix-1/home/nextcloud.nix
@@ -80,6 +80,6 @@ in
         sed -i 's/Listen 80/Listen 8080/' /etc/apache2/ports.conf
         sed -i 's/VirtualHost \*:80/VirtualHost \*:8080/' /etc/apache2/sites-available/000-default.conf
       '';
-    }
+    };
   };
 }
