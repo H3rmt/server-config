da6921110a74345bdb33fc6253d9ff12c5aa364a
Enrico Stemmer <enrico@h3rmt.zip>
2024-11-02 01:39:07 +0100

    fix: nginx config

--- main-nix-2/home/reverseproxy.nix
+++ main-nix-2/home/reverseproxy.nix
@@ -440,9 +440,8 @@ in
 
         include /etc/nginx/mime.types;
         default_type application/octet-stream;
-        log_format main '$remote_addr - $remote_user [$time_local] "$request" '
-                        '$status $body_bytes_sent "$http_referer" '
-                        '"$http_user_agent" "$http_x_forwarded_for" "$http3"';
+        log_format main '$remote_addr - [$time_local] "$request" '
+                        '"$http_user_agent" "$status" "$http3"';
         access_log /var/log/nginx/access.log main; 
         sendfile   on;
         tcp_nopush on;
