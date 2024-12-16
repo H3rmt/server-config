690fc9182fff62f4e7a43b8a241f9c68729bed7f
Enrico Stemmer <enrico@h3rmt.zip>
2024-12-16 17:20:44 +0100

    fix: update pup version

--- main-nix-1/home/puppeteer-sma.nix
+++ main-nix-1/home/puppeteer-sma.nix
@@ -1,6 +1,6 @@
 { lib, config, home, pkgs, clib, mainConfig, inputs, ... }:
 let
-  PUPPEREER_SMA_VERSION = "v0.1.3";
+  PUPPEREER_SMA_VERSION = "v0.1.4";
 
   SUNNY_PASSWORD = ''$(cat "${mainConfig.age.secrets.sunny_password.path}")'';
 in
