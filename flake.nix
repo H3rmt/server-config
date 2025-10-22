18624d72ed4f41b1f49d2f281228bd199a4c6062
Enrico Stemmer <enrico@h3rmt.zip>
2025-10-23 01:44:09 +0200

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,79 +1,71 @@
 {
-  description = "Server flake";
+  description = "Server flake (Headscale + K3s cluster)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    agenix-rekey.url = "github:oddlama/agenix-rekey";
-    agenix-rekey.inputs.nixpkgs.follows = "nixpkgs";
-    agenix.url = "github:ryantm/agenix";
-    # agenix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix-rekey = {
+      url = "github:oddlama/agenix-rekey";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      agenix,
-      agenix-rekey,
-      ...
-    }:
-    {
-      formatter.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.nixfmt-tree;
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
-      nixosConfigurations = {
-        # main-nix-1 = nixpkgs.lib.nixosSystem ({
-        #   system = "aarch64-linux";
-        #   specialArgs = { inherit inputs; };
-        #   modules = [
-        #     # agenix.nixosModules.default
-        #     # ./shared/config.nix
-        #     # ./main-nix-1/configuration.nix
-        #   ];
-        # });
-        # main-nix-2 = nixpkgs.lib.nixosSystem ({
-        #   system = "aarch64-linux";
-        #   specialArgs = { inherit inputs; };
-        #   modules = [
-        #     # agenix.nixosModules.default
-        #     # ./shared/config.nix
-        #     # ./main-nix-2/configuration.nix
-        #   ];
-        # });
-        raspi-1 = nixpkgs.lib.nixosSystem ({
-          system = "aarch64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./secrets.nix
-            ./base.nix
-            ./raspi-1.nix
-            ./k3s-worker.nix
-            agenix.nixosModules.default
-            agenix-rekey.nixosModules.default
+  outputs = inputs@{ self, nixpkgs, flake-parts, agenix, agenix-rekey, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { pkgs, system, ... }: {
+        formatter = pkgs.nixfmt-tree;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fzf
+            micro
+            agenix-rekey.packages.${system}.default
           ];
-        });
-        ovh-1 = nixpkgs.lib.nixosSystem ({
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./secrets.nix
-            ./base.nix
-            ./ovh-1.nix
-            ./k3s-root.nix
-            agenix.nixosModules.default
-            agenix-rekey.nixosModules.default
-          ];
-        });
+        };
       };
-      agenix-rekey = agenix-rekey.configure {
-        userFlake = self;
-        nixosConfigurations = self.nixosConfigurations;
-      };
-      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-        packages = [
-          agenix-rekey.packages.x86_64-linux.default
-          nixpkgs.legacyPackages.x86_64-linux.fzf
-          nixpkgs.legacyPackages.x86_64-linux.micro
-        ];
+
+      flake = {
+        nixosConfigurations = {
+          raspi-1 = nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            specialArgs = { inherit inputs; };
+            modules = [
+              ./secrets.nix
+              ./base.nix
+              ./raspi-1.nix
+              ./k3s-worker.nix
+              agenix.nixosModules.default
+              agenix-rekey.nixosModules.default
+            ];
+          };
+
+          ovh-1 = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = { inherit inputs; };
+            modules = [
+              ./secrets.nix
+              ./base.nix
+              ./ovh-1.nix
+              ./k3s-root.nix
+              agenix.nixosModules.default
+              agenix-rekey.nixosModules.default
+            ];
+          };
+        };
+
+        agenix-rekey = agenix-rekey.configure {
+          userFlake = self;
+          nixosConfigurations = self.nixosConfigurations;
+        };
       };
     };
 }
