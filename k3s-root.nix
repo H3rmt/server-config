8c178773fc27a066909b9c4f627514865588bc09
Enrico Stemmer <enrico@h3rmt.zip>
2025-10-23 01:23:58 +0200

    fix added more args to k3s servers

--- k3s-root.nix
+++ k3s-root.nix
@@ -9,14 +9,14 @@
         # Wait for Tailscale to be up
         "${pkgs.coreutils}/bin/sleep 5"
       ];
-      ExecStart = lib.mkForce "/bin/sh -c '${pkgs.k3s}/bin/k3s server \
+      ExecStart = lib.mkForce ''/bin/sh -c "${pkgs.k3s}/bin/k3s server \
         --tls-san=ovh-1.h3rmt.internal \
         --node-name=ovh-1 \
         --cluster-init \
         --token-file ${config.age.secrets.k3s.path} \
         --advertise-address=$(tailscale ip -4) \
-        --node-ip=$(tailscale ip -4)'
-      ";
+        --node-ip=$(tailscale ip -4)"
+      '';
     };
   };
 }
--- k3s-worker.nix
+++ k3s-worker.nix
@@ -9,12 +9,12 @@
         # Wait for Tailscale to be up
         "${pkgs.coreutils}/bin/sleep 5"
       ];
-      ExecStart = lib.mkForce "/bin/sh -c '${pkgs.k3s}/bin/k3s server \
+      ExecStart = lib.mkForce ''/bin/sh -c "${pkgs.k3s}/bin/k3s server \
         --server https://ovh-1.h3rmt.internal:6443 \
         --token-file ${config.age.secrets.k3s.path} \
         --node-name=raspi-1 \
-        --node-ip=$(tailscale ip -4)'
-      ";
+        --node-ip=$(tailscale ip -4)"
+      '';
     };
   };
 }
