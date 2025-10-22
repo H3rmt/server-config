45052a11a60a074fe393fccaeaf563c819bbfa22
Enrico Stemmer <enrico@h3rmt.zip>
2025-10-23 00:55:21 +0200

    fix added more args to k3s servers

--- k3s-worker.nix
+++ k3s-worker.nix
@@ -1,9 +1,22 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   services.k3s = {
     enable = true;
     role = "server";
     tokenFile = config.age.secrets.k3s.path;
     serverAddr = "https://ovh-1.h3rmt.internal:6443";
+    extraFlags = [
+      "--node-name=raspi-1"
+      "--node-ip=$${K3S_NODE_IP}"
+    ];
+  };
+  systemd.services.k3s-server = {
+    serviceConfig.ExecStartPre = [
+      # Wait for Tailscale to be up
+      "${pkgs.coreutils}/bin/sleep 5"
+    ];
+    environment = {
+      K3S_NODE_IP = "$(${pkgs.tailscale}/bin/tailscale ip -4 | head -n1)";
+    };
   };
 }
