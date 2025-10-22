ffd76c90edbaeeece5935e013da41586b3ad533e
Enrico Stemmer <enrico@h3rmt.zip>
2025-10-22 19:34:46 +0200

    remove debug arg from k3s

--- k3s-worker.nix
+++ k3s-worker.nix
@@ -4,9 +4,6 @@
     enable = true;
     role = "agent";
     tokenFile = config.age.secrets.k3s.file;
-    extraFlags = toString [
-      "--debug" # Optionally add additional args to k3s
-    ];
     serverAddr = "https://raspi-1.h3rmt.internal:6443";
   };
 }
