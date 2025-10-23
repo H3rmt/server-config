bd3b67d960743c074439d341968b8b6692d32ed5
Enrico Stemmer <enrico@h3rmt.zip>
2025-10-23 02:47:26 +0200

    make raspi agent instead of server

--- k3s-agent.nix
+++ k3s-agent.nix
@@ -9,7 +9,7 @@
         # Wait for Tailscale to be up
         "${pkgs.coreutils}/bin/sleep 5"
       ];
-      ExecStart = lib.mkForce ''/bin/sh -c "${pkgs.k3s}/bin/k3s server \
+      ExecStart = lib.mkForce ''/bin/sh -c "${pkgs.k3s}/bin/k3s agent \
         --server https://ovh-1.h3rmt.internal:6443 \
         --token-file ${config.age.secrets.k3s.path} \
         --node-name=raspi-1 \
