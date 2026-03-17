86fa5d3c49ea65a22340e4405d0c89208c133f62
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-17 23:22:37 +0100

    fix wg systemd networkd secret perms

--- home-1/secrets.nix
+++ home-1/secrets.nix
@@ -1,4 +1,6 @@
 { ... }:
 {
   age.secrets.wireguard_private.rekeyFile = ./secrets/wireguard_private.age;
+  age.secrets.wireguard_private.group = "systemd-network";
+  age.secrets.wireguard_private.mode = "640";
 }
