0540fc303e952565f45e499be81a3d2ba5633e16
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-18 17:34:41 +0100

    fix k3s extern ip

--- ovh-1/index.nix
+++ ovh-1/index.nix
@@ -43,8 +43,7 @@
     clusterInit = true;
     extraFlags = [
       "--flannel-iface=wg0 --tls-san=k3s-main.h3rmt.dev"
-      "--node-external-ip=${config.custom.server."ovh-1".public-ip-v4}"
-      "--node-external-ip=${config.custom.server."ovh-1".public-ip-v6}"
+      "--node-external-ip=${config.custom.server."ovh-1".public-ip-v4},${config.custom.server."ovh-1".public-ip-v6}"
     ];
   };
 }
