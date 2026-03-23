bd4ec21b53c2c2b2e7aecf193cb37ad1efb0dc2e
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-23 23:57:10 +0100

    added descheduler

--- ovh-1/index.nix
+++ ovh-1/index.nix
@@ -48,6 +48,7 @@
     extraFlags = [
       "--flannel-iface=wg0 --tls-san=k3s-main.h3rmt.dev"
       "--node-external-ip=${config.custom.server."ovh-1".public-ip-v4},${config.custom.server."ovh-1".public-ip-v6}"
+      "--kube-controller-manager-arg=pod-eviction-timeout=1m0s"
     ];
     manifests = {
       "hetzner-external-dns.yaml".source = config.age.secrets.hetzner-external-dns.path;
