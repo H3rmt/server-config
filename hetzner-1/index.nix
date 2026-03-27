80424f90bfdd5b4b7137f8f8eb30947316572336
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-27 16:35:59 +0100

    fix firewall in hetzner

--- hetzner-1/index.nix
+++ hetzner-1/index.nix
@@ -16,8 +16,18 @@
     enable = true;
     rejectPackets = true;
     interfaces."eth0" = {
-      allowedTCPPorts = [ ];
-      allowedUDPPorts = [ ];
+      allowedTCPPorts = [
+        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+        2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+        2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        443
+        80
+      ];
+      allowedUDPPorts = [
+        443
+        8472 # k3s, flannel: required if using multi-node for inter-node networking
+        51820 # wireguard
+      ];
     };
     trustedInterfaces = [ "wg0" ];
   };
