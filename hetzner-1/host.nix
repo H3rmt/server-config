4e03e5bec8d408853b498caa74a980060d42007d
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-27 15:08:36 +0100

    remove kernel modules from hetzner-1

--- hetzner-1/host.nix
+++ hetzner-1/host.nix
@@ -18,7 +18,7 @@
       efiSupport = false;
       device = "/dev/sda";
     };
-    kernelModules = [ "kvm-intel" ];
+    kernelModules = [ ];
     kernelParams = [ "boot.shell_on_fail" ];
     initrd.availableKernelModules = [
       # "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"
