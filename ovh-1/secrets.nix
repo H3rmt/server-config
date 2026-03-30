b83394143a5171e5db69aaf7bb477c56bfc12acd
Enrico Stemmer <enrico@h3rmt.dev>
2026-03-30 22:43:47 +0200

    added middleware for alerts

--- ovh-1/secrets.nix
+++ ovh-1/secrets.nix
@@ -10,4 +10,5 @@
   age.secrets.garage-rpc.rekeyFile = ./secrets/garage_rpc.age;
   age.secrets.garage-webui.rekeyFile = ./secrets/garage_webui.age;
   age.secrets.grafana-admin.rekeyFile = ./secrets/grafana-admin.age;
+  age.secrets.traefik-auth-secret-alerts.rekeyFile = ./secrets/traefik-auth-secret-alerts.age;
 }
