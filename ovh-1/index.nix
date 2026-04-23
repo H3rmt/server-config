9435d24bba01103a459817162d25464ae9444d0e
Enrico Stemmer <enrico@h3rmt.dev>
2026-04-23 09:33:25 +0200

    fix diskpools again

--- ovh-1/index.nix
+++ ovh-1/index.nix
@@ -54,7 +54,10 @@
       Type = "oneshot";
       RemainAfterExit = true;
       ExecStart = "${pkgs.util-linux}/bin/losetup --direct-io=on /dev/loop1 /mnt/mayastor.img";
+      # Create a persistent symlink that Mayastor will accept
+      ExecStartPost = "${pkgs.coreutils}/bin/ln -sf /dev/loop0 /dev/disk/by-id/loop-mayastor-raid";
       ExecStop = "${pkgs.util-linux}/bin/losetup -d /dev/loop1";
+      ExecStopPost = "${pkgs.coreutils}/bin/rm -f /dev/disk/by-id/loop-mayastor-raid";
     };
   };
   services.k3s = {
