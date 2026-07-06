48facb744711060d6cf5382462b07bdfaded732f
H3rmt <enrico@h3rmt.dev>
2026-07-06 21:25:01 +0200

    fix hetzner node name

--- hetzner-1/index.nix
+++ hetzner-1/index.nix
@@ -43,7 +43,7 @@
     enable = true;
     tokenFile = config.age.secrets.k3s.path;
     role = "server";
-    nodeName = config.networking.hostName;
+    nodeName = "${config.networking.hostName}.${config.networking.domain}";
     nodeLabel = [];
     clusterInit = false;
     serverAddr = "https://k3s-main.h3rmt.dev:6443";
